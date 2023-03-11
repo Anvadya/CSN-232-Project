d4fc1cf6ee49676b9dcdc978fe46c973d1344f2c
Anvadya Khare <58135621+Anvadya@users.noreply.github.com>
2023-03-11 16:51:34 +0530

    Driver code for Lamport code based synchronisation

---
+++ Q6/Lamport Clock based Synchronisation/lamportLockBasedSynchronisation.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <pthread.h>
+#include "messagePassing.h"
+
+#define NO_OF_OPERATIONS 8
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+int operations[NO_OF_PROCESSES+1][NO_OF_OPERATIONS];
+
+void *process();
+
+int main(){
+    initialise();
+    pthread_t processs[NO_OF_PROCESSES];
+    int pid[NO_OF_PROCESSES+1];
+    for(int i = 0; i < NO_OF_PROCESSES+1; ++i){
+        for(int j = 0; j < NO_OF_OPERATIONS; ++j){
+            operations[i][j] = i;
+        }
+    }
+    operations[1][4] = 2;
+    operations[2][2] = -1;
+    operations[2][4] = 1;
+    operations[1][6] = -2;
+    for(int i = 0; i < NO_OF_PROCESSES+1; ++i) pid[i] = i;
+    for(int i = 0; i < NO_OF_PROCESSES; ++i){
+        pthread_create(&processs[i], NULL, (void *)process, (void *)&pid[i+1]);
+    }
+    for(int i = 0; i < NO_OF_PROCESSES; ++i){
+        pthread_join(processs[i], NULL);
+    }
+}   
+
+void *process(void *args){
+    // printf("Start\n"); fflush(stdout);
+    int pid = *((int *)args);
+    int clock_timer = 0;
+    int data, tmpTime;
+    for(int i = 0; i < NO_OF_OPERATIONS; ++i){
+        if(operations[pid][i] == pid) ++clock_timer;
+        else{
+            if(operations[pid][i] > 0){
+                sendMessage(pid, operations[pid][i], pid);
+                sendMessage(pid, operations[pid][i], clock_timer);
+                printf("Writing data from %d to %d at time %d\n", pid, operations[pid][i], clock_timer);
+                fflush(stdout);
+                ++clock_timer;
+            }
+            else{
+                data = readMessage(-operations[pid][i], pid);
+                printf("Read data from %d by %d, data = %d\n", -operations[pid][i], pid, data);
+                fflush(stdout);
+                tmpTime = readMessage(-operations[pid][i], pid);
+                printf("Read clock time from %d by %d, clock time = %d\n", -operations[pid][i], pid, tmpTime);
+                fflush(stdout);
+                ++tmpTime;
+                clock_timer = MAX(clock_timer, tmpTime);
+                ++clock_timer;
+            }
+        }
+        printf("Clock for process: %d is %d\n", pid, clock_timer);
+        fflush(stdout);
+    }
+}
