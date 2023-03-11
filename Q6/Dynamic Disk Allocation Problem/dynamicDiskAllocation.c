d889f4ff2bdf3f373dc0127eb3af01e27b0ad70c
Anvadya Khare <58135621+Anvadya@users.noreply.github.com>
2023-03-11 12:18:55 +0530

    Adding Dynamic Disk Allocation driver code

---
+++ Q6/Dynamic Disk Allocation Problem/dynamicDiskAllocation.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "DiskLock.h"
+#include "semaphore.h"
+
+#define NO_OF_CPU 8
+const int MEM_RANGE = N;
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+void *cpuProcess();
+
+int main(){
+    build(0, 0, N-1);
+    pthread_t cpu[NO_OF_CPU];
+    int pids[NO_OF_CPU];
+    for(int i = 0; i < NO_OF_CPU; ++i) pids[i] = i+1;
+    for(int i = 0; i < NO_OF_CPU; ++i){
+        pthread_create(&cpu[i], NULL, (void *)cpuProcess, (void *)(&pids[i]));
+    }
+    for(int i = 0; i < NO_OF_CPU; ++i){
+        pthread_join(cpu[i], NULL);
+    }
+    return 0;
+}
+
+void* cpuProcess(void *args){
+    int pid = *((int *)args);
+    int t1 = rand()%MEM_RANGE;
+    int t2 = rand()%MEM_RANGE;
+    // int l = MIN(t1, t2);
+    // int r = MAX(t1, t2);
+    int l = 10, r = 25;
+    printf("Trying to allocate memory range from: %d to %d by process: %d \n", l, r, pid);
+    while(allocate(l,r) == 0) {printf("Failed to allocate memory to process %d, trying again......\n", pid); sleep(1);}
+    printf("Memory allocated succesfully to process %d.\n", pid);
+    sleep(1);
+    deallocate(l, r);
+    printf("Memory deallocated by process %d\n", pid);
+}
