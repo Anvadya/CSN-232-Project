9a505135bfc1fc982b37acc9cc90a8c3461a6c81
Rishika <101270384+rishu-3619@users.noreply.github.com>
2023-03-12 07:27:44 +0530

    Create infinite_buffer.h

---
+++ Q1/infinite_buffer.h
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+
+sem_t empty;
+sem_t full;
+int in = 0;
+int out = 0;
+int buffer[1000005]; //have to create a dynamic array
+pthread_mutex_t mutex;
+
+void *producer(void *p)
+{   
+    int item;
+    for(int i = 0; i < 1000005; i++) {
+        item = rand(); // Produce an random item
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+        buffer[in] = item;
+        printf("Producer %d: Insert Item %d at %d\n", *((int *)p),buffer[in],in);
+        in = (in+1);
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+    }
+}
+
+void *consumer(void *c)
+{   
+    for(int i = 0; i < 1000005; i++) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        int item = buffer[out];
+        printf("Consumer %d: Remove Item %d from %d\n",*((int *)c),item, out);
+        out = (out+1);
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+    }
+}
+
+
+int main() {
+    pthread_t pro[6],con[6];
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty,0,1000005);
+    sem_init(&full,0,0);
+
+    int a[6] = {1,2,3,4,5,6}; //Just used for numbering the producer and consumer
+
+    for(int i = 0; i < 6; i++) {
+        pthread_create(&pro[i], NULL, (void *)producer, (void *)&a[i]);
+    }
+  
+    for(int i = 0; i < 6; i++) {
+        pthread_create(&con[i], NULL, (void *)consumer, (void *)&a[i]);
+    }
+
+    for(int i = 0; i < 6; i++) {
+        pthread_join(pro[i], NULL);
+    }
+  
+    for(int i = 0; i < 6; i++) {
+        pthread_join(con[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+
+    return 0;
+    
+}
+
