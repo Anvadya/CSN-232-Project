1289fe9281f24077b8a1580710f94c877d0f0efd
KOMAL GUPTA <99733151+coolldude@users.noreply.github.com>
2023-03-15 17:33:12 +0530

    Update Producer_Consumer_Bounded.c
    added comments

--- Q1/Bounded Buffer/Producer_Consumer_Bounded.c
+++ Q1/Bounded Buffer/Producer_Consumer_Bounded.c
@@ -11,16 +11,16 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-sem_t full, empty;
-int buffer[buffer_size];
-int top = 0;
+sem_t full, empty; //Created two semaphores denoting full and empty buffers
+int buffer[buffer_size]; //buffer array containing items produced by producer
+int top = 0; //indicates top of stack
 pthread_mutex_t mutex;
 
 void *producer(); 
 void *consumer();
 
 //Stack operations
-void push(int i)
+void push(int i)  //implementation of push operation for stack
 {
    if (top >= buffer_size)
    {
@@ -29,7 +29,7 @@ void push(int i)
    buffer[top] = i;
    top++;
 }
-int pop(void)
+int pop(void)   //implementation of pop operation for stack
 {
    top--;
    if (top < 0)
@@ -41,22 +41,22 @@ int pop(void)
 
 int main()
 {
-    pthread_t p[no_p], c[no_c];
-    pthread_mutex_init(&mutex, NULL);
-    sem_init(&empty, 0, buffer_size);
-    sem_init(&full, 0, 0);
+    pthread_t p[no_p], c[no_c];  //defining two pthreads for producer and consumer
+    pthread_mutex_init(&mutex, NULL); 
+    sem_init(&empty, 0, buffer_size); //initialising empty semaphore as size of buffer
+    sem_init(&full, 0, 0);  //initialising full semaphore as 0
     int max = MAX(no_p, no_c);
-    int num[max];
+    int num[max];  //array representing no. of producers and consumers
     
     for (int i=0; i<no_p; i++)
     {
         num[i] = i+1;
-        pthread_create(&p[i], NULL, (void *)producer, (void *)&num[i]);
+        pthread_create(&p[i], NULL, (void *)producer, (void *)&num[i]); //creating pthread for producer
     }    
     for (int i=0; i<no_c; i++)
     {
         num[i] = i+1;
-        pthread_create(&c[i], NULL, (void *)consumer, (void *)&num[i]);
+        pthread_create(&c[i], NULL, (void *)consumer, (void *)&num[i]);   //creating pthread for consumer
     }
     
     for (int i=0; i<no_p; i++)
@@ -77,13 +77,13 @@ void *producer(void *p_no)
 {
     for (int i=0; i<max_prod; i++)
     {
-        int nextProd = rand()%100;
-        sem_wait(&empty);
+        int nextProd = rand()%100;   //item no. is generated using random generator
+        sem_wait(&empty);      //producer will have to wait if buffer is full i.e. empty is 0
         pthread_mutex_lock(&mutex);
-        push(nextProd);
+        push(nextProd);    //item no. is then pushed onto the top of stack
         printf("Producer %d: Insert Item %d\n", *((int *)p_no),nextProd);      
         pthread_mutex_unlock(&mutex);
-        sem_post(&full);
+        sem_post(&full);  //semaphore full will be incremented once an item is produced and inserted to stack
     }
 }
 
@@ -91,11 +91,11 @@ void *consumer(void *c_no)
 {
     for (int i=0; i<max_cons; i++)
     {        
-        sem_wait(&full);
+        sem_wait(&full);   //consumer will have to wait if the buffer is empty i.e. full is 0
         pthread_mutex_lock(&mutex);
-        int nextCons = pop();        
+        int nextCons = pop();    // consumer will consume the item which is on top of stack i.e. recently added
         printf("Consumer %d: Remove Item %d \n",*((int *)c_no),nextCons);
         pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
+        sem_post(&empty);  //semaphore empty will be incremented once an item is consumed as one item is removed
     }
 }
